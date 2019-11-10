674b716a0fb094152370918492c3449c74bd3db5
Nurbek Muktar <nurba.shorukov@gmail.com>
2019-11-11 02:06:09 +0600

    add new project

---
+++ temirio/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark></v-app-bar>
+
+    <v-content>
+      <Card />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Card from "./components/Card";
+
+export default {
+  name: "App",
+
+  components: {
+    Card
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
